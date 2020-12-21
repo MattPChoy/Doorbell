4ea084e7703501bf94467adf74d277baa9d8e79b
matthew.choy <matthew.choy@uqconnect.edu.au>
2020-12-21 11:47:54 +1000

    Fixed up method call

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,7 +1,7 @@
 import face_recognition as f
 
 def processImage(img):
-    image = f.load_image(img)
+    image = f.load_image_file(img)
 
     numberOfFaces = len(f.face_locations(image))
     print(F"There are {numberOfFaces} faces in the image")

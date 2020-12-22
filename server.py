3cdc769c83724ff9a7a5edc6e0980bfbe6396b0c
matthew.choy <matthew.choy@uqconnect.edu.au>
2020-12-23 09:13:09 +1000

    Modified the hostname of the server in the client file

--- server.py
+++ server.py
@@ -3,8 +3,10 @@ import socket
 import struct
 from PIL import Image
 
-# Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
-# all interfaces)
+# Start a socket listening for connections on 0.0.0.0:8000
+# (0.0.0.0) means listening on all interfaces
+print(f"Socket hostname: {socket.gethostname()}")
+
 server_socket = socket.socket()
 server_socket.bind(('0.0.0.0', 8000))
 server_socket.listen(0)
@@ -13,19 +15,23 @@ server_socket.listen(0)
 connection = server_socket.accept()[0].makefile('rb')
 try:
     while True:
-        # Read the length of the image as a 32-bit unsigned int. If the
-        # length is zero, quit the loop
+        # Read the ength of the image as a 32-bit unsigned int.
+        # If the length is 0 (stopped transmitting), exit out of the loop
+
         image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
         if not image_len:
             break
+    
         # Construct a stream to hold the image data and read the image
         # data from the connection
+
         image_stream = io.BytesIO()
         image_stream.write(connection.read(image_len))
-        # Rewind the stream, open it as an image with PIL and do some
-        # processing on it
-        image_stream.seek(0)
+
+        # Rewind the stream, open it as an image with PIL and do some processing on it
+        image_stream.seek(0) # Go back to the start of the image stream.
         image = Image.open(image_stream)
+
         print('Image is %dx%d' % image.size)
         image.verify()
         print('Image is verified')
